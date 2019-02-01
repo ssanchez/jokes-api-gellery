b66cdc1dcf2c189c8d8247814d4062f4cdf7fbce
Spencer Sanchez <spsanchez@wayfair.com>
2019-01-31 20:33:03 -0500

    Add button to pull dad joke

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  handleClick = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(({ joke }) => {
+        console.log(joke);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <article>
+        <button type="button" onClick={this.handleClick}>
+          get a joke
+        </button>
+      </article>
     );
   }
 }
